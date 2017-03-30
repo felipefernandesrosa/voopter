0b9c88d5e6ec3954b840757a4fafc96befe325fc
Felipe Rosa <felipe.rosa@ever.ag>
2017-03-30 14:00:15 -0300

    Implementando css e aplicando regras para gerar url 'Voopter'

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,77 +1,87 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('voopter')
-    .controller('MainController', MainController)
+    angular
+      .module('voopter')
+      .controller('MainController', MainController)
 
-  /** @ngInject */
-  function MainController($scope, $filter, $timeout, webDevTec, toastr) {
-    
-    var vm = this;
-    
-    console.log(vm);
-    
-    $scope.cities = [
-        {
-            "name" : "Rio De Janeiro",
-            "uf": "RJ",
-            "url_name" : "rio-de-janeiro",
-            "airport" : "GIG"
+    /** @ngInject */
+    function MainController($scope, $filter, $window) {
+      
+        $scope.cities = [
+            {
+                "name" : "Rio De Janeiro",
+                "uf": "RJ",
+                "url_name" : "rio-de-janeiro",
+                "airport" : "GIG"
+                
+            },
+            {
+                "name" : "São Paulo",
+                "uf": "SP",
+                "url_name" : "sao-paulo",
+                "airport"  : "GRU"
+            },
+            {
+                "name" : "Porto Alegre - POA",
+                "uf": "SP",
+                "url_name" : "porto-alegre",
+                "airport"  : "POA"
+            }
+        ];
+       
+       /**
+        * Calendar box
+        **/
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date(),
+            startingDay: 1
+        };
+        
+        $scope.open1 = function() {
+            $scope.popup1.opened = true;
+        };
+      
+        $scope.popup1 = { opened: false};
+       
+        $scope.open2 = function() {
+            $scope.popup2.opened = true;
+        };
+      
+        $scope.popup2 = { opened: false };
+        
+        /**
+         * Radio box
+         **/
+        
+        $scope.radioModel = 'one-way-flight';
+        
+        $scope.Add = function() {
             
-        },
-        {
-            "name" : "São Paulo",
-            "uf": "SP",
-            "url_name" : "sao-paulo",
-            "airport"  : "GRU"
-        },
-        {
-            "name" : "Porto Alegre - POA",
-            "uf": "SP",
-            "url_name" : "porto-alegre",
-            "airport"  : "POA"
+            var city_origin = JSON.parse($scope.vp.city_origin);
+            var city_destination = JSON.parse($scope.vp.city_destination);
+            
+            
+            if($scope.vp.date_leave != undefined){
+              var date_leave = '#dl%5B%5D='+$filter('date')($scope.vp.date_leave,"yyyyMMdd");  
+            }else{
+              console.log('to aqui');
+              var date_leave = '';
+            }
+            
+            if($scope.vp.date_return != undefined){
+              var date_return = '&dr%5B%5D='+$filter('date')($scope.vp.date_return,"yyyyMMdd");  
+            }else{
+              console.log('to aqui2');
+              var date_return = '';
+            }
+            
+            $window.open('https://voopter.com.br/passagens-aereas-de-'+city_origin.url_name+'/'+city_origin.airport+'/para-'+city_destination.url_name+'/'+city_destination.airport+date_leave+date_return+'&na=1&nc=0&ni=0&sn=true&pop=true','_blank');
+            
         }
-    ];
-   
-    $scope.dateOptions = {
-        formatYear: 'yy',
-        maxDate: new Date(2020, 5, 22),
-        minDate: new Date(),
-        startingDay: 1
-    };
-    
-    $scope.format = 'yyyy/MM/dd';
-    
-    $scope.open1 = function(par) {
-        console.log(par);
-        $scope.popup1.opened = true;
-    };
-  
-    $scope.popup1 = {
-        opened: false
-    };
-   
-    $scope.open2 = function() {
-        $scope.popup2.opened = true;
-    };
-  
-    $scope.popup2 = {
-        opened: false
-    };
-     
-    $scope.Add = function() {
-        
-        var city_origin = JSON.parse($scope.vp.city_origin);
-        var city_destination = JSON.parse($scope.vp.city_destination);
-        var date_leave = $filter('date')($scope.vp.date_leave,"yyyyMMdd");
-        var date_return = $filter('date')($scope.vp.date_return,"yyyyMMdd");
-        
-        console.log('https://voopter.com.br/passagens-aereas-de-'+city_origin.url_name+'/'+city_origin.airport+'/para-'+city_destination.url_name+'/'+city_destination.airport+'#dl%5B%5D='+date_leave+'&dr%5B%5D='+date_return+'&na=1&nc=0&ni=0&sn=true&pop=true');
-        
     }
-    
-  }
     
     
 
